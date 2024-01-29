cb546dae7e8682d94c8428cf432b649d083dc04f
ilhamibnu <e41210178@student.polije.ac.id>
2024-01-30 00:01:23 +0700

    update templating, feat controller & routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NilaiKeyakinanController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Auth Controller
+Route::get('/', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
+
+# Dashboard Controller
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+# Nilai Keyakinan Controller
+Route::get('/nilai-keyakinan', [NilaiKeyakinanController::class, 'index']);
