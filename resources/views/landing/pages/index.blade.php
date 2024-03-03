cf551eef8e0eb4351e4dbf1ffb66e45cb1bf57b6
ilhamibnu <e41210178@student.polije.ac.id>
2024-03-03 13:49:30 +0700

    .

--- resources/views/landing/pages/index.blade.php
+++ resources/views/landing/pages/index.blade.php
@@ -1,13 +1,13 @@
 @extends('landing.layout.main')
 
 @section('content')
-<div class="hero-wrap js-fullheight" style="background-image: url('{{ asset('landing/images/bg_1.jpg') }}');" data-stellar-background-ratio="0.5">
+<div class="hero-wrap js-fullheight" style="background-image: url('https://images.unsplash.com/photo-1533743983669-94fa5c4338ec?q=80&w=1992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
             <div class="col-md-11 ftco-animate text-center">
-                <h1 class="mb-4">Highest Quality Care For Pets You'll Love </h1>
-                <p><a href="#" class="btn btn-info mr-md-4 py-3 px-4">Learn more <span class="ion-ios-arrow-forward"></span></a></p>
+                <h1 class="mb-4">Sistem Pakar Diagnosa Penyakit Kucing</h1>
+                <p><a href="http://127.0.0.1:8000/auth/google" class="btn btn-info mr-md-4 py-3 px-4">Diagonosa Sekarang <span class="ion-ios-arrow-forward"></span></a></p>
             </div>
         </div>
     </div>
@@ -15,7 +15,7 @@
 
 <section class="ftco-section bg-light ftco-no-pt ftco-intro">
     <div class="container">
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-md-4 d-flex align-self-stretch px-4 ftco-animate">
                 <div class="d-block services active text-center">
                     <div class="icon d-flex align-items-center justify-content-center">
@@ -52,7 +52,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </section>
 
@@ -60,7 +60,7 @@
     <div class="container">
         <div class="row d-flex no-gutters">
             <div class="col-md-5 d-flex">
-                <div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0" style="background-image:url({{ asset('landing/images/about-1.jpg') }});">
+                <div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0" style="background-image:url({{ asset('https://images.unsplash.com/photo-1579168765467-3b235f938439?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxjYXR8ZW58MHx8MHx8fDA%3D') }});">
                 </div>
             </div>
             <div class="col-md-7 pl-md-5 py-md-5">
@@ -102,121 +102,121 @@
     </div>
 </section>
 
-<section class="ftco-counter" id="section-counter">
+{{-- <section class="ftco-counter" id="section-counter">
     <div class="container">
         <div class="row">
 
         </div>
     </div>
-</section>
-
+</section> --}}
+{{--
 <section class="ftco-section bg-light ftco-faqs">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 order-md-last">
                 <div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0" style="background-image:url({{ asset('landing/images/about.jpg') }});">
-                    <a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex justify-content-center align-items-center">
-                        <span class="fa fa-play"></span>
-                    </a>
-                </div>
-                <div class="d-flex mt-3">
-                    <div class="img img-2 mr-md-2" style="background-image:url({{ asset('landing/images/about-2.jpg') }});"></div>
-                    <div class="img img-2 ml-md-2" style="background-image:url({{ asset('landing/images/about-3.jpg') }});"></div>
+<a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex justify-content-center align-items-center">
+    <span class="fa fa-play"></span>
+</a>
+</div>
+<div class="d-flex mt-3">
+    <div class="img img-2 mr-md-2" style="background-image:url({{ asset('landing/images/about-2.jpg') }});"></div>
+    <div class="img img-2 ml-md-2" style="background-image:url({{ asset('landing/images/about-3.jpg') }});"></div>
+</div>
+</div>
+
+<div class="col-lg-6">
+    <div class="heading-section mb-5 mt-5 mt-lg-0">
+        <h2 class="mb-3">Frequently Asks Questions</h2>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+    </div>
+    <div id="accordion" class="myaccordion w-100" aria-multiselectable="true">
+        <div class="card">
+            <div class="card-header p-0" id="headingOne">
+                <h2 class="mb-0">
+                    <button href="#collapseOne" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="true" aria-controls="collapseOne">
+                        <p class="mb-0">How to train your pet dog?</p>
+                        <i class="fa" aria-hidden="true"></i>
+                    </button>
+                </h2>
+            </div>
+            <div class="collapse show" id="collapseOne" role="tabpanel" aria-labelledby="headingOne">
+                <div class="card-body py-3 px-0">
+                    <ol>
+                        <li>Far far away, behind the word mountains</li>
+                        <li>Consonantia, there live the blind texts</li>
+                        <li>When she reached the first hills of the Italic Mountains</li>
+                        <li>Bookmarksgrove, the headline of Alphabet Village</li>
+                        <li>Separated they live in Bookmarksgrove right</li>
+                    </ol>
                 </div>
             </div>
+        </div>
 
-            <div class="col-lg-6">
-                <div class="heading-section mb-5 mt-5 mt-lg-0">
-                    <h2 class="mb-3">Frequently Asks Questions</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+        <div class="card">
+            <div class="card-header p-0" id="headingTwo" role="tab">
+                <h2 class="mb-0">
+                    <button href="#collapseTwo" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseTwo">
+                        <p class="mb-0">How to manage your pets?</p>
+                        <i class="fa" aria-hidden="true"></i>
+                    </button>
+                </h2>
+            </div>
+            <div class="collapse" id="collapseTwo" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="card-body py-3 px-0">
+                    <ol>
+                        <li>Far far away, behind the word mountains</li>
+                        <li>Consonantia, there live the blind texts</li>
+                        <li>When she reached the first hills of the Italic Mountains</li>
+                        <li>Bookmarksgrove, the headline of Alphabet Village</li>
+                        <li>Separated they live in Bookmarksgrove right</li>
+                    </ol>
                 </div>
-                <div id="accordion" class="myaccordion w-100" aria-multiselectable="true">
-                    <div class="card">
-                        <div class="card-header p-0" id="headingOne">
-                            <h2 class="mb-0">
-                                <button href="#collapseOne" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="true" aria-controls="collapseOne">
-                                    <p class="mb-0">How to train your pet dog?</p>
-                                    <i class="fa" aria-hidden="true"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div class="collapse show" id="collapseOne" role="tabpanel" aria-labelledby="headingOne">
-                            <div class="card-body py-3 px-0">
-                                <ol>
-                                    <li>Far far away, behind the word mountains</li>
-                                    <li>Consonantia, there live the blind texts</li>
-                                    <li>When she reached the first hills of the Italic Mountains</li>
-                                    <li>Bookmarksgrove, the headline of Alphabet Village</li>
-                                    <li>Separated they live in Bookmarksgrove right</li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+        </div>
 
-                    <div class="card">
-                        <div class="card-header p-0" id="headingTwo" role="tab">
-                            <h2 class="mb-0">
-                                <button href="#collapseTwo" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseTwo">
-                                    <p class="mb-0">How to manage your pets?</p>
-                                    <i class="fa" aria-hidden="true"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div class="collapse" id="collapseTwo" role="tabpanel" aria-labelledby="headingTwo">
-                            <div class="card-body py-3 px-0">
-                                <ol>
-                                    <li>Far far away, behind the word mountains</li>
-                                    <li>Consonantia, there live the blind texts</li>
-                                    <li>When she reached the first hills of the Italic Mountains</li>
-                                    <li>Bookmarksgrove, the headline of Alphabet Village</li>
-                                    <li>Separated they live in Bookmarksgrove right</li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div>
+        <div class="card">
+            <div class="card-header p-0" id="headingThree" role="tab">
+                <h2 class="mb-0">
+                    <button href="#collapseThree" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseThree">
+                        <p class="mb-0">What is the best grooming for your pets?</p>
+                        <i class="fa" aria-hidden="true"></i>
+                    </button>
+                </h2>
+            </div>
+            <div class="collapse" id="collapseThree" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="card-body py-3 px-0">
+                    <ol>
+                        <li>Far far away, behind the word mountains</li>
+                        <li>Consonantia, there live the blind texts</li>
+                        <li>When she reached the first hills of the Italic Mountains</li>
+                        <li>Bookmarksgrove, the headline of Alphabet Village</li>
+                        <li>Separated they live in Bookmarksgrove right</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
 
-                    <div class="card">
-                        <div class="card-header p-0" id="headingThree" role="tab">
-                            <h2 class="mb-0">
-                                <button href="#collapseThree" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseThree">
-                                    <p class="mb-0">What is the best grooming for your pets?</p>
-                                    <i class="fa" aria-hidden="true"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div class="collapse" id="collapseThree" role="tabpanel" aria-labelledby="headingTwo">
-                            <div class="card-body py-3 px-0">
-                                <ol>
-                                    <li>Far far away, behind the word mountains</li>
-                                    <li>Consonantia, there live the blind texts</li>
-                                    <li>When she reached the first hills of the Italic Mountains</li>
-                                    <li>Bookmarksgrove, the headline of Alphabet Village</li>
-                                    <li>Separated they live in Bookmarksgrove right</li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card">
-                        <div class="card-header p-0" id="headingFour" role="tab">
-                            <h2 class="mb-0">
-                                <button href="#collapseFour" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseFour">
-                                    <p class="mb-0">What are those requirements for sitting pets?</p>
-                                    <i class="fa" aria-hidden="true"></i>
-                                </button>
-                            </h2>
-                        </div>
-                        <div class="collapse" id="collapseFour" role="tabpanel" aria-labelledby="headingTwo">
-                            <div class="card-body py-3 px-0">
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                            </div>
-                        </div>
-                    </div>
+        <div class="card">
+            <div class="card-header p-0" id="headingFour" role="tab">
+                <h2 class="mb-0">
+                    <button href="#collapseFour" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseFour">
+                        <p class="mb-0">What are those requirements for sitting pets?</p>
+                        <i class="fa" aria-hidden="true"></i>
+                    </button>
+                </h2>
+            </div>
+            <div class="collapse" id="collapseFour" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="card-body py-3 px-0">
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
                 </div>
             </div>
         </div>
     </div>
-</section>
+</div>
+</div>
+</div>
+</section> --}}
 
 <section class="ftco-counter" id="section-counter">
     <div class="container">
