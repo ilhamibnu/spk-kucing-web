5ccc050c04b88d9dab73d03b6269352ced87a6f6
ilhamibnu <e41210178@student.polije.ac.id>
2024-05-09 23:20:05 +0700

    .

--- resources/views/landing/pages/diagnosa.blade.php
+++ resources/views/landing/pages/diagnosa.blade.php
@@ -51,12 +51,33 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <div class="mt-4">
-                            <img src="{{ asset('foto_gejala/'.$value->foto) }}" alt="" class="img-fluid">
-                        </div>
-                        <div class="mt-3 text-center">
-                            <a href="{{ $value->link_penjelasan }}" class="btn btn-primary">Detail</a>
-                        </div>
 
+                        </div>
+                        <div class="mt-4 text-center">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Detail{{ $value->id }}">
+                                Detail
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal -->
+            <div class="modal fade" id="Detail{{ $value->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title fs-5" id="exampleModalLabel">Detail</h4>
+
+                        </div>
+                        <div class="modal-body">
+                            <img src="{{ asset('foto_gejala/'.$value->foto) }}" alt="" class="img-fluid">
+                            <div class="text-center mt-3">
+                                <a href="{{ $value->link_penjelasan }}" class="btn btn-primary">Link</a>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
