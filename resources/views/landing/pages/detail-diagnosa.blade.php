2fe074cb25337152481b87c3b139cff14572bfb5
ilhamibnu <e41210178@student.polije.ac.id>
2024-05-20 21:13:37 +0700

    .

--- resources/views/landing/pages/detail-diagnosa.blade.php
+++ resources/views/landing/pages/detail-diagnosa.blade.php
@@ -28,6 +28,11 @@
                 <h5 class="font-weight-bold">Kesimpulan</h5>
                 <p>Berdasarkan dari gejala yang kamu pilih atau alami juga berdasarkan Role/Basis aturan yang sudah ditentukan oleh seorang pakar penyakit maka perhitungan Algoritma Certainty Factor mengambil nilai CF yang paling pinggi yakni <b>{{ number_format(unserialize($riwayat->cf_max)[0], 3) }} ({{ number_format(unserialize($riwayat->cf_max)[0], 3) * 100 }}%)</b> yaitu <b>{{ unserialize($riwayat->cf_max)[1] }}</b></p>
                 <br>
+                <br>
+                <h5 class="font-weight-bold">Deskripsi</h5>
+                <p>
+                    {{ $riwayat->penyakit->deskripsi }}
+                </p>
                 <h5 class="font-weight-bold">Solusi</h5>
                 <p>
                     {{ $riwayat->penyakit->solusi }}
